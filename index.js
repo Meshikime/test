eba35d5f3cfe0a39034c29f9c9a42ea3f7c5a61d
Kirill <xhatter7@gmail.com>
2020-05-28 10:23:26 +0300

    Это наш первый комит

---
+++ index.js
@@ -0,0 +1,52 @@
+ 'use strict';
+
+
+
+// console.log(leftBorderWidth);
+// var leftBorderWidth = 1;
+// var string = "Hello";
+
+// let second = 2;
+// const pi = 3.14;
+
+// console.log(3 / 0);
+// console.log(string * 3);
+
+// let computer = {
+//     motherboard: "asus",
+//     CPU: "AMD",
+//     isAudio: true
+// };
+// // console.log(computer["motherboard"]);
+// let arr = ['ps.png','Aleks.jpg','Kirill.bmp'];
+// console.log(arr[0]);
+// //alert("Hello World");
+
+// // let answer = confirm("Are you ready?");
+// // console.log(answer);
+
+
+// let answer = prompt("Are you eighteen?", "Yes");
+// console.log(answer);
+
+//  console.log(typeof(String(null)));
+// console.log(typeof( ""+5));
+//let ans = prompt("Hello?", '');
+// let switcher = null;
+// if (switcher) {
+//     console.log("Working...");
+// }
+// switcher = 1;
+// if (switcher) {
+//     console.log("Working...");
+// }
+
+
+console.log(5%4);
+console.log(1===2);
+
+
+let isChecked = true,
+    isClose = false;
+
+    console.log(isChecked && isClose);
